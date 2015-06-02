e56228c77b8fdacc28dcdc96aba5b56567c7c0f6
maechabin <mail@chab.in>
2015-06-02 20:57:30 +0900

    add jQuery

--- build.js
+++ build.js
@@ -10,7 +10,7 @@ formApp.controller("FormController", ["$scope", "$http", function ($scope, $http
   $scope.submit = function (user) {
 
     this.master = angular.copy(user);
-    console.log(this.master);
+    //console.log(this.master);
 
     $http({
       method: "GET",
@@ -19,15 +19,17 @@ formApp.controller("FormController", ["$scope", "$http", function ($scope, $http
       })
       .success(function (data, status, headers, config) {
         $scope.message = "success";
-        console.log($scope.message);
-        console.dir(arguments);
-        angular.element(".form").html("お問い合わせを受け付けました。<br>ありがとうございます。");
+        location.hash = "#" + $scope.message;
+        //console.log($scope.message);
+        //console.dir(arguments);
+        angular.element(".form__list").html("お問い合わせを受け付けました。<br>ありがとうございます。");
       })
       .error(function (data, status, headers, config) {
         $scope.message = "failed";
-        console.log($scope.message)
-        console.dir(arguments);
-        angular.element(".form").html("お問い合わせの送信に失敗しました。<br>時間をおいてから再度お試しください。");
+        location.hash = "#" + $scope.message;
+        //console.log($scope.message);
+        //console.dir(arguments);
+        angular.element(".form__list").html("お問い合わせの送信に失敗しました。<br>時間をおいてから再度お試しください。");
       });
 
   };
